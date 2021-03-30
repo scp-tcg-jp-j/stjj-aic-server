0b125d53d9dda81950c2a07a3019bf81350d3c20
smctrl <stjj@kokosema.net>
2021-03-30 23:44:19 +0900

    ログをコンソールではなくファイルに出す

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,13 @@
 import winston = require('winston')
+import DailyRotateFile = require('winston-daily-rotate-file')
 
 // ロガー生成
 export const logger = winston.createLogger({
     transports: [
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.timestamp({
-                    format: "YYYY-MM-DD HH:mm:ss"
-                })
-            )
-        }),
+        new DailyRotateFile({
+            dirname: '/home/stjj-aic/log',
+            filename: 'node.log-%DATE%',
+            datePattern: 'YYYY-MM-DD',
+        })
     ],
 });
